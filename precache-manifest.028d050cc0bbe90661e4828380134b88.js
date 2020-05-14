b941213247409a9bd0d72b4b59075613949f95f4
RohitKeswani <rhareshk@gmail.com>
2020-05-14 16:45:16 -0400

    Updates

---
+++ precache-manifest.028d050cc0bbe90661e4828380134b88.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3a9fef60dfc76fbfbbd2f945a53e0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d652d117bed294c7f434",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "1a26901c482c45111442",
+    "url": "/static/css/main.2f1aaf9a.chunk.css"
+  },
+  {
+    "revision": "d652d117bed294c7f434",
+    "url": "/static/js/2.beefc8a6.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.beefc8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a26901c482c45111442",
+    "url": "/static/js/main.53c6fd90.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "faf8e106072c43e84b84197f8f0231d1",
+    "url": "/static/media/AirlineOnTimePerformance.faf8e106.pdf"
+  },
+  {
+    "revision": "371d482467410aff203af0f9770e1e69",
+    "url": "/static/media/CapstoneReport.371d4824.pdf"
+  },
+  {
+    "revision": "dac7c9bf48b16248328670d1238fcd79",
+    "url": "/static/media/Rohit_Keswani_Resume.dac7c9bf.pdf"
+  },
+  {
+    "revision": "f1349c4f4f685a1bab797f342771f520",
+    "url": "/static/media/mypic1.f1349c4f.png"
+  },
+  {
+    "revision": "a946a054ef3c0ece438372eb06962b16",
+    "url": "/static/media/mypic3.a946a054.png"
+  },
+  {
+    "revision": "a6f594ca268e17e4c63e339b373ee338",
+    "url": "/static/media/project-bg.a6f594ca.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.02d79e8b0deacf3cfbce04cca6a646f2.js"
+  "/precache-manifest.028d050cc0bbe90661e4828380134b88.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
