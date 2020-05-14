52a821c43be6ffae27a3b58fafd7ad30a8bf6984
RohitKeswani <rhareshk@gmail.com>
2020-05-14 16:43:18 -0400

    Updates

---
+++ precache-manifest.02d79e8b0deacf3cfbce04cca6a646f2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e3f2c5456c48a69b22b1363a0e96c8d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d652d117bed294c7f434",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "04a6a47576f7a7fcf29e",
+    "url": "/portfolio/static/css/main.4027f945.chunk.css"
+  },
+  {
+    "revision": "d652d117bed294c7f434",
+    "url": "/portfolio/static/js/2.beefc8a6.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.beefc8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a6a47576f7a7fcf29e",
+    "url": "/portfolio/static/js/main.35e0ce19.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "faf8e106072c43e84b84197f8f0231d1",
+    "url": "/portfolio/static/media/AirlineOnTimePerformance.faf8e106.pdf"
+  },
+  {
+    "revision": "371d482467410aff203af0f9770e1e69",
+    "url": "/portfolio/static/media/CapstoneReport.371d4824.pdf"
+  },
+  {
+    "revision": "dac7c9bf48b16248328670d1238fcd79",
+    "url": "/portfolio/static/media/Rohit_Keswani_Resume.dac7c9bf.pdf"
+  },
+  {
+    "revision": "f1349c4f4f685a1bab797f342771f520",
+    "url": "/portfolio/static/media/mypic1.f1349c4f.png"
+  },
+  {
+    "revision": "a946a054ef3c0ece438372eb06962b16",
+    "url": "/portfolio/static/media/mypic3.a946a054.png"
+  },
+  {
+    "revision": "a6f594ca268e17e4c63e339b373ee338",
+    "url": "/portfolio/static/media/project-bg.a6f594ca.jpg"
+  }
+]);
